dbacb2ce4ac16c1554464612f7ff978b3607fad4
Dimesha Wijerathna <dimesha99kanchani@gmail.com>
2026-01-03 02:48:47 +0530

    inset the sample data to DB

---
+++ Backend/seed.sql
@@ -0,0 +1,9 @@
+USE farminvest;
+
+INSERT INTO investments (farmer_name, amount, crop) VALUES
+('Dimesha Wijerathna', 15000.00, 'Green Apple'),
+('Sanadru Nethmin', 8000.00, 'Baby Potato'),
+('Sesmi Seeniya', 12000.00, 'Ginger'),
+('daran weerakon', 9500.00, 'Rice');
+
+-- sample data use for testing the DB
